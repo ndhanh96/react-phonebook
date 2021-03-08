d7d90ae2a6571616a6e8a699c687bc5b20131509
ndhanh96 <ndhanh96@gmail.com>
2021-03-08 15:36:27 +0700

    initial upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
 function App() {
+  const [name, setName] = useState([]);
+  const [all, setAll] = useState([]);
+  const [isClicked, setClicked] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/api/persons/")
+      .then((res) => res.json())
+      .then((res) => {
+        res.map((person) => {
+          setName((oldName) => [
+            ...oldName,
+            { id: person.id, name: person.name, number: person.number },
+          ]);
+        });
+      });
+  }, []);
+
+  const showALL = () => {
+    setName((name) => all);
+    setClicked(!isClicked);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {name.map((n) => (
+        <div key={n.id}>
+          <p>
+            {n.name} {n.number}
+          </p>
+          {isClicked ? (
+            <FilterMe
+              setAll={setAll}
+              setName={setName}
+              name={name}
+              isClicked={isClicked}
+              setClicked={setClicked}
+              id={n.id}
+            />
+          ) : (
+            <></>
+          )}
+        </div>
+      ))}
+      {!isClicked ? <button onClick={showALL}>Show ALL</button> : <></>}
+      <FormSender />
     </div>
+  );
+}
+
+function FilterMe({ id, name, isClicked, setClicked, setAll, setName }) {
+  function handleChange() {
+    setClicked(!isClicked);
+    setAll((all) => name);
+    setName((name) => name.filter((n) => n.id === id));
+  }
+  return <button onClick={handleChange}>Click me</button>;
+}
+
+function FormSender(props) {
+  const [nameData, setNameData] = useState("");
+  const [numberData, setNumberData] = useState("");
+
+  async function postData(url , data) {
+    console.log(url);
+    try {
+      const response = await axios.post(url, data); 
+      console.log(response);
+    } catch (err) {
+      console.log(err); 
+    }
+  }
+
+  function handleSubmit(e) {
+    if (nameData && numberData) {
+      postData("http://localhost:3001/api/persons/", {
+        name: nameData,
+        number: numberData,
+      });
+    } else {
+      alert("missing name and number");
+    }
+    setNameData("");
+    setNumberData("");
+    e.preventDefault();
+  }
+
+  function handleNameChange(e) {
+    setNameData(e.target.value);
+  }
+
+  function handleNumberChange(e) {
+    setNumberData(e.target.value);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input value={nameData} onChange={handleNameChange} type="text" />
+      </label>
+      <label>
+        Number:
+        <input value={numberData} onChange={handleNumberChange} type="text" />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
   );
 }
 
